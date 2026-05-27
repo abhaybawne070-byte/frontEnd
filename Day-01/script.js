db00e98703b522c301cf51d34dac12146642cf61
abhay bawne <abhaybawne070@gmail.com>
2026-05-27 10:33:52 +0530

    Refactor script.js to comment out image and anchor manipulation examples

--- Day-01/script.js
+++ Day-01/script.js
@@ -23,19 +23,36 @@
         // a.setAttribute("href","https://www.google.com")
         
         // setAttribute :-
-        let img = document.querySelector("img")
-        img.setAttribute(
-            "src",
-            "https://images.unsplash.com/photo-1777026059641-53bcbd04f194?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8"
-        )
+        // let img = document.querySelector("img")
+        // img.setAttribute(
+        //     "src",
+        //     "https://images.unsplash.com/photo-1777026059641-53bcbd04f194?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8"
+        // )
 
         // getAttribute :-
         // let a = document.querySelector("a")
         // console.log(a.getAttribute("href"))
 
         // removeAttribute :-
-        let a = document.querySelector("a")
-        a.removeAttribute("href")
+        // let a = document.querySelector("a")
+        // a.removeAttribute("href")
 
+// Daynamic dom manipulation - 
 
-        
+        // createElement-
+                // let elementName = document.createElement("tagName")
+                // let h1 = document.createElement("h1")
+                // console.log(h1) 
+
+        // append / prepend -  append use for last and prepend use for first
+                // let h1 = document.createElement("h1")
+                // h1.textContent=("hello aanu")
+                // document.body.append(h1)
+                // document.querySelector("body").prepend(h1)
+
+        // remove - 
+                // let h1 = document.createElement("h1")
+                // h1.textContent = "hello aanu kasi ho"
+                // document.body.append(h1)
+                // h1.remove()
+
